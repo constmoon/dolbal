4f6d9dac7730d62cd7a58e3723d6d9ab5080c8ff
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-11 15:23:12 +0900

    feat(api): api에서 모든 데이터를 처리하도록 변경

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,17 +1,37 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import accList, { getAccType } from '../data';
 import { KAKAO } from '../settings';
 
 const headers = {
   'Authorization': 'KakaoAK ' + KAKAO.APP_KEY
 }
 
-const getAccidentPosition = async (acc) => {
-  const { grs80tm_x, grs80tm_y } = acc;
+const getAccidentPosition = async (accident) => {
+  const { grs80tm_x, grs80tm_y } = accident;
   const url = `${KAKAO.BASE_URL}/transcoord.json?x=${grs80tm_x}&y=${grs80tm_y}&input_coord=WTM&output_coord=WGS84`
-  const position = await axios.get(url, { headers })
-    .then((response) => response.data.documents[0])
-    .catch((error) => { console.log(error) });
-  return { ...acc, ...position };
+  try {
+    const response = await axios(url, { headers });
+    const position = response.data.documents[0];
+    return { ...accident, ...position };
+  }
+  catch(error) {
+    console.log(error)
+  }
 }
 
-export { getAccidentPosition };
+const getAccidentList = () => {
+  const [accidentList, setAccidentList] = useState([]);
+
+  useEffect(() => {
+    // TODO: 컴포넌트 바깥에서 변환이 이루어지는 안좋은 설계 수정 
+    async function setAccList() {
+      const accidentWithPosition = await Promise.all(accList.map(getAccidentPosition));
+      setAccidentList(accidentWithPosition);
+    }
+    setAccList();
+  },[]);
+  return [accidentList];
+}
+
+export { getAccidentList };
