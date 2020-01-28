f1132b09a3ff157ae8144234b1d1834bae7794fc
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-28 23:15:03 +0900

    chore: sample data 파일 분리

--- frontend/src/api/sampledata.js
+++ frontend/src/api/sampledata.js
@@ -1,4 +1,4 @@
-const accList = [
+const SAMPLE_API_RESULT = [
   {
     acc_id: "718620",
     occr_date: "20191123",
@@ -53,60 +53,6 @@ const accList = [
   }
 ];
 
-const accTypeList = [
-  {
-    acc_type: "A01",
-    acc_type_nm: "교통사고"
-  },
-  {
-    acc_type: "A02",
-    acc_type_nm: "차량고장"
-  },
-  {
-    acc_type: "A03",
-    acc_type_nm: "보행사고"
-  },
-  {
-    acc_type: "A04",
-    acc_type_nm: "공사"
-  },
-  {
-    acc_type: "A05",
-    acc_type_nm: "낙하물"
-  },
-  {
-    acc_type: "A06",
-    acc_type_nm: "버스사고"
-  },
-  {
-    acc_type: "A07",
-    acc_type_nm: "지하철사고"
-  },
-  {
-    acc_type: "A08",
-    acc_type_nm: "화재"
-  },
-  {
-    acc_type: "A09",
-    acc_type_nm: "기상/재난"
-  },
-  {
-    acc_type: "A10",
-    acc_type_nm: "집회및행사"
-  },
-  {
-    acc_type: "A11",
-    acc_type_nm: "기타"
-  },
-  {
-    acc_type: "A12",
-    acc_type_nm: "제보"
-  },
-  {
-    acc_type: "A13",
-    acc_type_nm: "단순정보"
-  }
-];
-
-export const getAccType = (type) => accTypeList.find(acc => acc.acc_type === type).acc_type_nm;
-export default accList;
+export {
+  SAMPLE_API_RESULT
+}
