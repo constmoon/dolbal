9995358be36ec4d2049961fbb42028dff556a0da
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-29 18:56:30 +0900

    feat: 기존 데이터에서 필요한 값만 분리하여 새 배열 리턴

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -6,6 +6,21 @@ import { GRS80toWGS84 } from 'api/transcoord';
 
 const getAccidentList = () => {
   const [accidentList, setAccidentList] = useState([]);
+const getTypeName = (type) => {
+  const name = ACCIDENT_TYPES.filter(item => item.acc_type === type).map(item => item.acc_type_nm)
+  return name.toString();
+};
+
+const setAccidentFields = async (accident) => {
+  const { longitude, latitude } = await GRS80toWGS84(accident.grs80tm_x, accident.grs80tm_y);
+  return {
+    type: getTypeName(accident.acc_type),
+    description: accident.acc_info,
+    longitude: longitude,
+    latitude: latitude,
+  };
+};
+
 
   useEffect(() => {
     // TODO: 컴포넌트 바깥에서 변환이 이루어지는 안좋은 설계 수정 
