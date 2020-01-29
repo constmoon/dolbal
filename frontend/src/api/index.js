f1b9370f0b42a27ea0688d4c7ad9cbc1f60fcc00
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-29 18:53:57 +0900

    feat: api/index.js에서 transcoord 역할을 제거

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,24 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import accList, { getAccType } from '../data';
-import { KAKAO } from '../settings';
-
-const headers = {
-  'Authorization': 'KakaoAK ' + KAKAO.APP_KEY
-}
-
-const getAccidentPosition = async (accident) => {
-  const { grs80tm_x, grs80tm_y } = accident;
-  const url = `${KAKAO.BASE_URL}/transcoord.json?x=${grs80tm_x}&y=${grs80tm_y}&input_coord=WTM&output_coord=WGS84`
-  try {
-    const response = await axios(url, { headers });
-    const position = response.data.documents[0];
-    return { ...accident, ...position };
-  }
-  catch(error) {
-    console.log(error)
-  }
-}
 
 const getAccidentList = () => {
   const [accidentList, setAccidentList] = useState([]);
