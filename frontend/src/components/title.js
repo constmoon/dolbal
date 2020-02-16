2625e30dd26cde9bcde8251960ffef4039f7bc49
constmoon <jiiw.kang@gmail.com>
2020-02-16 09:45:02 +0900

    feat: 사이트 소개 내용 추가

--- frontend/src/components/title.js
+++ frontend/src/components/title.js
@@ -3,13 +3,14 @@ import React from 'react';
 const SiteInformation = () => {
   // TODO: description 문구 추가하기
   return (
-    <>
-      <h1 className="section-title">DOLBAL informations</h1>
-      <style jsx>{`
-      .section-title {
-        padding: 0 0.6rem;
-        height: 3.4rem;
-        line-height: 3.4rem;
+    <section className="site-info">
+      <h1 className="title">DOLBAL informations</h1>
+      <p className="desc">서울시 도로에서 발생하는 돌발 상황을 실시간으로 제공하여 안전한 외출을 도와줍니다.</p>
+    <style jsx>{`
+      .site-info {
+        padding: 0.6rem;
+      }
+      .title {
         font-size: 1.65rem;
       }
     `}</style>
